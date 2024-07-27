6d308c57c3c583d6c852644d6ee6f080a8af22a7
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-07-27 15:18:16 +0200

    ho dovuto togliere la parte html - perche non riuscivo a collegarla in mdo funzionante-ma anche togliendola non mi funziona lo stesso e non capisco perche

--- Palindromo/script.js
+++ Palindromo/script.js
@@ -1,1 +1,23 @@
-const 
+/*
+CONSEGNA
+Palidroma
+Chiedere all'utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+//Domanda all utente 
+const inputParola = prompt("Scrivi una parola per verificare se è palindroma");
+
+function verifiPalindroma(word){
+    let notify = 'la parola è palindroma'
+    let k = word.length; -1; 
+    
+    for( let i=0; i< word.lenght; i++){
+        
+        else (word[i] != word[k] K>i){
+            console.log('i è diverso da k')
+            notify = 'la parola non è palindroma'
+        }
+    }
+    return notify;
+}
+let result = verifiPalindroma(inputParola);
